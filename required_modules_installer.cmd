04dca9c81b19f4e951092da7a3934682bab04c2b
ocvcoin <90601304+ocvcoin@users.noreply.github.com>
2021-10-22 00:37:27 +0000

    Add files via upload

---
+++ required_modules_installer.cmd
@@ -0,0 +1,21 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        
+		
+		
+		pip3 install opencv-python==3.2.0.6 --force-reinstall
+		pip3 install bech32 --force-reinstall
+		
+		
+		
+    ) else (
+        echo Please right click and click run as administrator.
+    )
+	echo Press any key to exit...
+    pause >nul
